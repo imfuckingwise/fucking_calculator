c9e49bc9b914f01f3f8ccbb7d7928fef7ba1af19
wise <42545954+imfuckingwise@users.noreply.github.com>
2024-02-26 02:23:59 +0800

    Update and rename fucking_calculator.py to main.py

--- main.py
+++ main.py
@@ -23,19 +23,20 @@ days = 365
 
 # 計算存款收益
 deposit_amount_twd, deposit_profit_twd, deposit_amount_usdt, deposit_profit_usdt = calculate_deposit_profit(deposit_amount_usdt, deposit_rate, usdt_to_twd_exchange_rate_deposit, usdt_to_twd_exchange_rate_withdraw, days)
-print("存款本金：", deposit_amount_twd, "TWD，", deposit_amount_usdt, "USDT")
-print("存款利息：", deposit_profit_twd, "TWD，", deposit_profit_usdt, "USDT")
+print("【存款本金：", deposit_amount_twd, "TWD，", deposit_amount_usdt, "USDT】")
+print("【存款利息：", deposit_profit_twd, "TWD，", deposit_profit_usdt, "USDT】")
 
 principal = float(input("請輸入貸款金額（單位：TWD）："))
 annual_interest_rate = float(input("請輸入年利率（例如，0.07代表7%）："))
 
 # 計算貸款利息
 total_interest = calculate_revolving_credit_interest(principal, annual_interest_rate, days)
-print("貸款利息：", total_interest, "TWD")
+print("【貸款本金：", principal, "TWD】")
+print("【貸款利息：", total_interest, "TWD】")
 
 # 計算淨收益
 net_deposit_twd = deposit_amount_twd + deposit_profit_twd
 net_deposit_usdt = deposit_amount_usdt + deposit_profit_usdt
 net_profit_twd = net_deposit_twd - (principal + total_interest)
 net_profit_usdt = net_deposit_usdt - (net_profit_twd / usdt_to_twd_exchange_rate_withdraw)
-print("淨收益：", net_profit_twd, "TWD，", net_profit_usdt, "USDT")
+print("【淨收益：", net_profit_twd, "TWD，", net_profit_usdt, "USDT】")
